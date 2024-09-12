df153efb66b8ad7c9b7b7cace79b6c6ef4bed857
Noy-98 <noy.services01@gmail.com>
2024-09-13 00:12:09 +0800

    Front-End part 1

--- app/src/main/java/com/itech/innogas/ForgotPasswordActivity.kt
+++ app/src/main/java/com/itech/innogas/ForgotPasswordActivity.kt
@@ -2,32 +2,26 @@ package com.itech.innogas
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.bumptech.glide.Glide
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MainActivity : AppCompatActivity() {
-
-    private val splash_time: Long = 2000
-
+class ForgotPasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_forgot_password)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val next = findViewById<FloatingActionButton>(R.id.next)
+        val backBttn = findViewById<FloatingActionButton>(R.id.go_back_bttn)
 
-        next.setOnClickListener {
+        backBttn.setOnClickListener {
             val intent = Intent (this, LoginActivity::class.java)
             startActivity(intent)
         }
--- app/src/main/java/com/itech/innogas/LoginActivity.kt
+++ app/src/main/java/com/itech/innogas/LoginActivity.kt
@@ -2,33 +2,34 @@ package com.itech.innogas
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.bumptech.glide.Glide
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MainActivity : AppCompatActivity() {
-
-    private val splash_time: Long = 2000
-
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val next = findViewById<FloatingActionButton>(R.id.next)
+        val forgotPasswordBttn = findViewById<TextView>(R.id.forgot_password_bttn)
+        val signupBttn = findViewById<TextView>(R.id.sign_up_bttn)
 
-        next.setOnClickListener {
-            val intent = Intent (this, LoginActivity::class.java)
+        forgotPasswordBttn.setOnClickListener {
+            val intent = Intent (this, ForgotPasswordActivity::class.java)
+            startActivity(intent)
+        }
+
+        signupBttn.setOnClickListener {
+            val intent = Intent (this, SignupActivity::class.java)
             startActivity(intent)
         }
     }
